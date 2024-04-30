d1974d519081f4790de567f73ce3597c476fb11e
chaewonkim <kimlove9182@gmail.com>
2024-05-01 03:09:41 +0900

    chore: 초기세팅

---
+++ chaewon/src/main/java/com/example/chaewon/ChaewonApplication.java
@@ -0,0 +1,13 @@
+package com.example.chaewon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaewonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaewonApplication.class, args);
+	}
+
+}
