08dc5f71344df8f0f6db8553ea6479a4e33365ad
chaewonkim <kimlove9182@gmail.com>
2024-05-02 19:22:25 +0900

    feat: 1주차 과제 업로드

---
+++ chaewon/src/main/java/com/example/chaewon/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.example.chaewon.config;
+
+import com.example.chaewon.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableMethodSecurity()
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity httpSecurity
+//            AuthorizationRequestRepository<OAuth2AuthorizationRequest> requestAuthorizationRequestRepository
+    ) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(
+                        authorizeHttpRequests ->
+                                authorizeHttpRequests
+                                        .requestMatchers(
+                                                new AntPathRequestMatcher("/login"),
+                                                new AntPathRequestMatcher("/login/oauth2/google"),
+                                                new AntPathRequestMatcher("/")
+                                        ).permitAll()
+                                        .anyRequest().authenticated()
+                )
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout((logoutConfig) -> logoutConfig.logoutSuccessUrl("/"))
+                .oauth2Login(oAuth2LoginConfigurer ->
+                        oAuth2LoginConfigurer
+//                                .authorizationEndpoint(authorizationEndpointConfig ->
+//                                        authorizationEndpointConfig
+//                                                .baseUri("/home")
+//                                                .authorizationRequestRepository(requestAuthorizationRequestRepository))
+                                .redirectionEndpoint(redirectionEndpointConfig ->
+                                        redirectionEndpointConfig
+                                                .baseUri("/login/oauth2/code/google"))
+                                .userInfoEndpoint(userInfoEndpointConfig ->
+                                        userInfoEndpointConfig
+                                                .userService(customOAuth2UserService)
+                                )
+                );
+        return httpSecurity.build();
+    }
+
+//    @Bean
+//    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> httpSessionOAuth2AuthorizationRequestRepository() {
+//        return new HttpSessionOAuth2AuthorizationRequestRepository();
+//    }
+
+//    @Bean
+//    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+//        return new DefaultOAuth2UserService();
+//    }
+}
---
+++ chaewon/src/main/java/com/example/chaewon/service/dto/OAuthAttributes.java
@@ -0,0 +1,53 @@
+package com.example.chaewon.service.dto;
+
+import com.example.chaewon.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes,
+                           String nameAttributeKey, String name,
+                           String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    // OAuth2User에서 반환하는 사용자 정보는 Map
+    // 따라서 값 하나하나를 변환해야 한다.
+    public static OAuthAttributes of(String registrationId,
+                                     String userNameAttributeName,
+                                     Map<String, Object> attributes) {
+
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    // 구글 생성자
+    private static OAuthAttributes ofGoogle(String usernameAttributeName,
+                                            Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(usernameAttributeName)
+                .build();
+    }
+
+    public Member toEntity() {
+        return Member.create(name, email, picture);
+    }
+}
